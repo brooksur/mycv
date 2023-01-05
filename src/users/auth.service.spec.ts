ecfabb209a052f89372582ed06e06530157c72a6
Brooks <brooks@pdrutah.com>
2023-01-05 09:47:34 -0700

    added tests for auth service

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,13 +5,25 @@ import { UsersService } from './users.service'
 import { User } from './user.entity'
 
 describe('AuthService', () => {
-  let service: AuthService, fakeUsersService: Partial<UsersService>
+  let service: AuthService,
+    fakeUsersService: Partial<UsersService>,
+    users: User[]
 
   beforeEach(async () => {
+    users = []
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        return Promise.resolve(users.filter((user) => user.email === email))
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 10000),
+          email,
+          password,
+        } as User
+        users.push(user)
+        return Promise.resolve(user)
+      },
     }
 
     const module = await Test.createTestingModule({
@@ -32,26 +44,40 @@ describe('AuthService', () => {
   })
 
   it('creates a new user with a salted and hashed password', async () => {
-    const user = await service.signup('email@email.com', 'password')
+    const [email, password] = ['a', '1']
+    const user = await service.signup(email, password)
     expect(user).toHaveProperty('id', expect.any(Number))
-    expect(user).toHaveProperty('email', 'email@email.com')
-    expect(user.password).not.toEqual('password')
+    expect(user).toHaveProperty('email', email)
+    expect(user.password).not.toEqual(password)
     const [salt, hash] = user.password.split('.')
     expect(salt).toBeDefined()
     expect(hash).toBeDefined()
   })
 
   it('throws an error if user signs up with email that is in use', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User])
-    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+    const [email, password] = ['a', '1']
+    await service.signup(email, password)
+    await expect(service.signup(email, password)).rejects.toThrow(
       BadRequestException,
     )
   })
 
   it('throws if signin is called with an unused email', async () => {
-    await expect(
-      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
-    ).rejects.toThrow(NotFoundException)
+    const [email, password] = ['a', '1']
+    await service.signup(email, password)
+    await expect(service.signin('a', '2')).rejects.toThrow(NotFoundException)
+  })
+
+  it('throws if an invalid password is provided', async () => {
+    const [email, password] = ['a', '1']
+    await service.signup(email, password)
+    await expect(service.signin('a', '2')).rejects.toThrow(NotFoundException)
+  })
+
+  it('returns a user if correct password is provided', async () => {
+    const [email, password] = ['a', '1']
+    await service.signup(email, password)
+    const user = await service.signin('a', '1')
+    expect(user).toBeDefined()
   })
 })
